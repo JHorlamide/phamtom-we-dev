7e462a1e39022562f354eefc5a9b6405524d82ad
olamide jubril <olamidejubril68@gmail.com>
2023-07-20 15:00:37 +0100

    Testing offline features

--- next.config.js
+++ next.config.js
@@ -1,11 +1,41 @@
-module.exports = {
+const withOffline = require("next-offline");
+
+module.exports = withOffline({
   reactStrictMode: false,
   eslint: {
-    ignoreDuringBuilds: true,
+    ignoreDuringBuilds: true
   },
   images: {
     domains: [
-      'res.cloudinary.com'
-    ],
+      "res.cloudinary.com"
+    ]
   },
-}
+  target: "serverless",
+  workboxOpts: {
+    swDest: "static/service-worker.js",
+    runtimeCaching: [
+      {
+        urlPattern: /^https?.*/,
+        handler: "NetworkFirst",
+        options: {
+          cacheName: "offlineCache",
+          expiration: {
+            maxEntries: 200
+          }
+        }
+      }
+    ]
+  }
+});
+
+// module.exports = {
+//   reactStrictMode: false,
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+//   images: {
+//     domains: [
+//       'res.cloudinary.com'
+//     ],
+//   },
+// }
